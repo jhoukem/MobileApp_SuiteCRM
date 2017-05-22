d716512ef4d9e307678d9afe3813f793f2bfd705
jhouwe <jhouwe@exelcia-it.com>
2017-05-22 16:24:19 -0400

    Added updateProspectList test + included enzyme module for shallow rendering

--- __tests__/components/ProspectListScreen/test.ProspectListScreen.jsx
+++ __tests__/components/ProspectListScreen/test.ProspectListScreen.jsx
@@ -1,5 +1,6 @@
-import 'react-native';
+import { ActivityIndicator } from 'react-native';
 import React from 'react';
+import { shallow, mount } from 'enzyme';
 import { ProspectListScreen } from '../../../app/components/ProspectListScreen/ProspectListScreen'
 
 // Note: test renderer must be required after react-native.
@@ -15,14 +16,22 @@ var arg = {
 }
 
 var prospectListMock = require('./prospectListMock.json');
+var prospectUpdateMock = require('./prospectUpdateMock.json');
 
+var findProspectById = (prospect, idToMatch) => {
+    return prospect.name_value_list.id.value == idToMatch;
+}
+
+afterEach(() => {
+  // Reload the default configuration after each test.
+  prospectUpdateMock = require('./prospectUpdateMock.json');
+});
 
 describe('ProspectListScreen', () => {
     it('renders without crashing', () => {
        const tree = renderer.create(
        	<ProspectListScreen jest={{list: prospectListMock}} navigation={arg}/>
        	);
-
     });
   	it('display the search list with the correct values', () => {
        const tree = renderer.create(
@@ -40,8 +49,37 @@ describe('ProspectListScreen', () => {
         instance.state.prospectSearch[0].name_value_list.first_name.value).toBe("prospect 6 jean");
        expect(instance.state.prospectSearch[1].name_value_list.last_name.value + " " +
         instance.state.prospectSearch[1].name_value_list.first_name.value).toBe("jean jaures ");
-
     });
-    
+    it('update a prospect from the list', () => {
+       const tree = renderer.create(
+        <ProspectListScreen jest={{list: prospectListMock}} navigation={arg}/>
+        );
+       const instance = tree.getInstance();
+       instance.updateProspectList(prospectUpdateMock);
+       var updatedProspectIndex = instance.state.prospectList.findIndex((item) => findProspectById(item, prospectUpdateMock.id.value)); 
+       var updatedProspect = instance.state.prospectList[updatedProspectIndex].name_value_list;
+       
 
+       // Ensure that the prospectList is updated with the corrects values.
+       expect(updatedProspect.first_name.value).toEqual(prospectUpdateMock.first_name.value);
+       expect(updatedProspect.last_name.value).toEqual(prospectUpdateMock.last_name.value);
+       expect(updatedProspect.description.value).toEqual(prospectUpdateMock.description.value);
+    });
+    it('delete a prospect from the list', () => {
+       const tree = renderer.create(
+        <ProspectListScreen jest={{list: prospectListMock}} navigation={arg}/>
+        );
+       const instance = tree.getInstance();
+       var listSize = instance.state.prospectList.length;
+       // Set the deletion flag.
+       prospectUpdateMock.deleted.value = 1; 
+
+       instance.updateProspectList(prospectUpdateMock);
+       var updatedProspectIndex = instance.state.prospectList.findIndex((item) => findProspectById(item, prospectUpdateMock.id.value)); 
+       
+
+       // Ensure that the prospectList is updated with the corrects values.
+       expect(instance.state.prospectList.length).toBe(listSize - 1);
+       expect(updatedProspectIndex).toBe(-1);
+    });
 });
